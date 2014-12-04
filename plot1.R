eebbacac95ff8e0de2370557912abd9bfc97e4f8
jrozen <jonas.rozenich@gmail.com>
2014-12-04 23:32:02 +0100

    Fixed transparent background

--- plot1.R
+++ plot1.R
@@ -1,6 +1,23 @@
-ds<-read.csv("household_power_consumption.txt", sep=";")
-ds<-ds[ds$Date=="1/2/2007"|ds$Date=="2/22007",]
-ds$Date<-as.Date(ds$Date, "%d/%m/%Y")
-ds$Global_active_power<-as.numeric(ds$Global_active_power)
-hist(ds$Global_active_power, breaks=12, 
+## The household_power_consumption.txt should be in the working direcory
+ds <- read.csv("household_power_consumption.txt", sep=";", 
+      colClasses = c("character", 
+                    "character", 
+                    "numeric",
+                    "numeric",
+                    "numeric",
+                    "numeric",
+                    "numeric", 
+                    "numeric",
+                    "numeric"),
+      na.strings="?")
+
+## Since the dataset is large, filter the Date vector as string
+dsf<-ds[ds$Date=="1/2/2007"|ds$Date=="2/2/2007",]
+rm(ds)
+
+## Create a png device and plot a histogram over global active power
+plot.new()
+png(filename = "plot1.png", width = 480, height = 480, units = "px", bg="transparent")
+hist(dsf$Global_active_power, breaks=12, 
      xlab="Global active power (kilowatts)", main="Global active power", col="red")
+dev.off()
